7b471f226520fddc4a8586b5df4965be0e5234b8
shahpershahin <shahpershahin@gmail.com>
2024-01-04 13:11:39 +0530

    secondcommit

--- rssagg.go
+++ rssagg.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -22,6 +23,21 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness) //healtz is just a name which makes me understand that weather the server is running or not
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
